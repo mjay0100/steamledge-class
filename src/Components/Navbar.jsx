cd3bd1b0d6390743cfbdb578df39d9aa67a45919
mustapha jay <mustaphajay2@gmail.com>
2022-06-26 23:21:16 +0100

    updated styles to elements

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <section className="flex justify-between items-center ">
+    <section className="flex justify-between items-center w-[80%] mx-auto ">
       <h1 className="text-2xl font-bold">
         <span className="text-blue-600">C</span>
         <span className="text-yellow-400">la</span>
         <span className="text-pink-300">ss.</span>
       </h1>
-      <ul className="flex gap-[1.5rem] text-[#ccc]">
+      <ul className="flex text-[#8d8c8c] gap-[1.5rem]">
         <li>Buy a course</li>
         <li>success</li>
         <li>Categories</li>
         <li>Pricing</li>
       </ul>
-      <button className="py-2 px-3 rounded-md border border-slate-200">
+      <button className="py-2 px-3 rounded-md border hover:bg-gray-800 hover:text-white transition duration-500 border-slate-200">
         Get started
       </button>
     </section>
