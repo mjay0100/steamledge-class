3954b0867c1a4a75e89e554fdc4a4b3e13b67a3d
mustapha jay <mustaphajay2@gmail.com>
2022-06-27 21:27:22 +0100

    added first card

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,5 +1,6 @@
 import woman from "../images/woman.png";
-import { BsPersonFill } from "react-icons/bs";
+
+import Card from './Card'
 
 const Content = () => {
   return (
@@ -28,7 +29,7 @@ const Content = () => {
           </button>
         </div>
 
-       
+        <Card/>
       </div>
       <div className="h-[294px] w-[200px] bg-lime-400 rounded-t-3xl rounded-b-2xl absolute left-[60%] right-0 top-[200px] rotate-12 "></div>
       <div className="h-[400px] w-[200px] bg-gray-900 rounded-t-3xl rounded-b-2xl absolute left-[80%] right-0 top-[100px] rotate-12 "></div>
