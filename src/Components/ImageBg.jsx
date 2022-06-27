b031a920cba8298070470e98aa5cb21213775bc6
mustapha jay <mustaphajay2@gmail.com>
2022-06-27 23:35:28 +0100

    added background design to woman image

--- src/Components/ImageBg.jsx
+++ src/Components/ImageBg.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const ImageBg = () => {
-  return <div>ImageBg</div>;
+  return <>
+   <div className="h-[294px] w-[200px] bg-lime-400 rounded-t-3xl rounded-b-2xl absolute left-[60%] right-0 top-[180px] rotate-12 "></div>
+      <div className="h-[400px] w-[200px] bg-gray-900 rounded-t-3xl rounded-b-2xl absolute left-[80%] right-0 top-[89px] rotate-12 "></div>
+  </>;
 };
 
 export default ImageBg;
