dd449a53784ef5fcfcebe0d63d0413cf6dd238ad
mustapha jay <mustaphajay2@gmail.com>
2022-06-28 00:06:56 +0100

    added media query

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <section className="flex justify-between items-center w-[80%] mx-auto ">
+    <section className="flex justify-between items-center w-[80%] mx-auto relative">
       <h1 className="text-2xl font-bold">
         <span className="text-blue-600">C</span>
         <span className="text-yellow-400">la</span>
         <span className="text-pink-300">ss.</span>
       </h1>
-      <ul className="flex text-[#8d8c8c] gap-[1.5rem]">
+      <ul className="hidden md:flex text-[#8d8c8c] gap-[1rem]">
         <li>Buy a course</li>
         <li>success</li>
         <li>Categories</li>
         <li>Pricing</li>
       </ul>
-      <button className="py-2 px-3 rounded-md border hover:bg-gray-800 hover:text-white transition duration-500 border-slate-200">
+      <button className="hidden lg:block py-2 px-3 rounded-md border hover:bg-gray-800 hover:text-white transition duration-500 border-slate-200">
         Get started
       </button>
+      <span className="absolute right-0 md:hidden text-2xl">
+        <FaBars />
+      </span>
     </section>
   );
 };
