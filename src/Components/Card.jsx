a0d69a61921ea20e17462b1b402f89a3fc164348
mustapha jay <mustaphajay2@gmail.com>
2022-06-27 23:41:36 +0100

    added all the card to a component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,30 @@
 import { BsPersonFill } from "react-icons/bs";
 
 const Card = () => {
-  return <div>card</div>;
+  return (
+    <div className="flex gap-[2rem] z-10 pointer">
+      <div className="flex gap-[1rem] items-center mt-[2rem] p-4 rounded-md shadow-sm shadow-slate-500">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-gray-800">Rate your experience</h1>
+          <input type="range" name="" id="" />
+        </div>
+        <span className="text-4xl">
+          <BsPersonFill />
+        </span>
+      </div>
+      <div className="flex gap-[1.2rem] items-center mt-[2rem] p-4 rounded-md shadow-sm shadow-slate-500 z-10 bg-white">
+        <div className="flex gap-2 items-baseline">
+          <div className="w-[10px] h-[30px] bg-gray-800 rounded-t-2xl"></div>
+          <div className="w-[10px] h-[45px] bg-yellow-500 rounded-t-2xl"></div>
+          <div className="w-[10px] h-[20px] bg-pink-500 rounded-t-2xl"></div>
+          <div className="w-[10px] h-[24px] bg-gray-800 rounded-t-2xl"></div>
+          <div className="w-[10px] h-[45px] bg-yellow-500 rounded-t-2xl"></div>
+        </div>
+        <h1 className="text-gray-800">Total Courses</h1>
+        <div className="p-4 text-white rounded-[50%] bg-gray-800">1K+</div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
