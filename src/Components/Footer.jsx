e5970efa37bac780d0c2a063756b1aad78b9d37a
mustapha jay <mustaphajay2@gmail.com>
2022-06-28 10:36:35 +0100

    added responsiveness

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,20 +3,26 @@ import { SiUdemy } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <section className="flex justify-between w-1/2 text-[#ccc] font-semibold mx-auto mt-[1.5rem] mb-[1rem] text-lg">
-      <div className="flex items-center gap-2">
-        <FaTrophy />
-        <h1 className="font-normal">unacademy</h1>
-      </div>
-      <h1>coursera</h1>
-      <h1 className="">
+    <section className="block  text-center md:flex justify-between w-4/6 text-[#ccc] font-semibold mx-auto mt-[1.5rem] mb-[1rem] text-lg">
+      <h1 className=" justify-center mb-2 md:justify-start items-center flex gap-2">
+        {" "}
+        <span>
+          <FaTrophy />
+        </span>{" "}
+        unacademy
+      </h1>
+      <h1 className="mb-2">coursera</h1>
+      <h1 className="mb-2">
         SKILL <br />
         SHare.
       </h1>
-      <div className="flex items-center gap-2">
-        <SiUdemy />
-        <h1>Udemy</h1>
-      </div>
+      <h1 className=" justify-center md:justify-start items-center flex gap-2">
+        <span>
+          {" "}
+          <SiUdemy />
+        </span>
+        Udemy
+      </h1>
     </section>
   );
 };
